72f2c1fe4bfa73131bafe6107cb227335bc673d2
Wasiul Islam Sakif <sakif049@gmail.com>
2026-09-01 14:36:45 +0600

    add: structure

---
+++ lib/sites/minimalisto/features/shell/shell.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Shell extends StatelessWidget {
+  const Shell({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
